a8f9cd2cb09d5c1d771d170d621dfd167c48e24b
Arthur Petroff <apetbrz@gmail.com>
2025-11-09 17:04:47 -0500

    fixed margins, added footer

---
+++ app/components/shared/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <div className="w-full p-2">
+            <span className="prose prose-stone dark:prose-invert">made by </span>
+            <a href="https://apetbrz.dev/">https://apetbrz.dev/</a>
+        </div>
+    );
+}
--- app/components/shared/Loading.tsx
+++ app/components/shared/Loading.tsx
@@ -1,7 +1,9 @@
 export default () => {
     return (
-        <div className="w-max mx-auto my-16">
-            <p className="prose prose-2xl">Loading...</p>
+        <div className="w-full p-16 min-h-128">
+            <div className="mx-auto w-min">
+                <p className="prose prose-2xl prose-stone dark:prose-invert">Loading...</p>
+            </div>
         </div>
     );
 }
