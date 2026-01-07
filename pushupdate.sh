b81fc943a89dd1142a932472a5aa3014bf4759a9
arthur petroff pi <apetbrz@gmail.com>
2026-01-07 13:22:17 -0500

    name change in docker script

--- pushupdate.sh
+++ pushupdate.sh
@@ -4,11 +4,11 @@ read -p "This will shut down and delete the existing container, okay? [y/N] " co
 [ "${conf,,}" = "y" ] || exit
 
 echo -n "Shutting down existing container: "
-docker -v kill fissures-app-new
+docker -v kill void-seer
 echo -n "Deleting existing container: "
-docker -v rm fissures-app-new
+docker -v rm void-seer
 echo "[Building latest version]"
-docker build -t fissures-app-new .
+docker build -t void-seer .
 echo "
 [Starting latest version]"
-docker run --name 'fissures-app-new' -p '4001:4001' -d fissures-app-new:latest
+docker run --name 'void-seer' -p '4001:4001' -d void-seer:latest
