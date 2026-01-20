607b6b54d81b725603729646aa41228ef775d24d
Arthur Petroff Desktop <apetbrz@gmail.com>
2026-01-20 17:22:29 -0500

    footer to one line

--- app/components/shared/Footer.tsx
+++ app/components/shared/Footer.tsx
@@ -1,9 +1,7 @@
 export default function Footer() {
     return (
         <div className="w-full p-2">
-            <span className="prose prose-stone dark:prose-invert">Thanks to <a href="https://github.com/WFCD">https://github.com/WFCD</a>.</span>
-            <br />
-            <span className="prose prose-stone dark:prose-invert">Not affiliated with Digital Extremes. Warframe is a registered trademark of Digital Extremes Ltd.</span>
+            <span className="prose prose-stone dark:prose-invert">Thanks to <a href="https://github.com/WFCD">https://github.com/WFCD</a>. Not affiliated with Digital Extremes. Warframe is a registered trademark of Digital Extremes Ltd.</span>
         </div>
     );
 }
