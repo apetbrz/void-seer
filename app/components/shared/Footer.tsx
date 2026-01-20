8f596c1a7fd03f2585f52d7ac2a47fff67a225e8
Arthur Petroff Desktop <apetbrz@gmail.com>
2026-01-20 16:43:19 -0500

    added credits footer

--- app/components/shared/Footer.tsx
+++ app/components/shared/Footer.tsx
@@ -1,8 +1,9 @@
 export default function Footer() {
     return (
         <div className="w-full p-2">
-            <span className="prose prose-stone dark:prose-invert">made by </span>
-            <a href="https://apetbrz.dev/">https://apetbrz.dev/</a>
+            <span className="prose prose-stone dark:prose-invert">Thanks to <a href="https://github.com/WFCD">https://github.com/WFCD</a>.</span>
+            <br />
+            <span className="prose prose-stone dark:prose-invert">Not affiliated with Digital Extremes. Warframe is a registered trademark of Digital Extremes Ltd.</span>
         </div>
     );
 }
